d24b8f77bedf27cd51fa680abea6d88b03c12710
Moka Yomoto <psp2ipso2moka@gmail.com>
2017-10-21 17:46:18 +0900

    Update N_Sumple.cs

--- Assets/Scripts/N_Sample.cs
+++ Assets/Scripts/N_Sample.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class N_Sample : MonoBehaviour {
-
+	private float origin;
+	private float per;
+	private float result;
 	// Use this for initialization
 	void Start () {
-		
+		origin = 100f;
+		per = 0.8f;
+		result = 0.0f;
+		Magouke ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Magouke() {
+		for (int i = 0; i < 10; i++) {
+			result = origin * per;
+			Debug.Log (result);
+			origin = result;
+		}
 	}
 }
