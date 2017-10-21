9f76aa3a12b163e6a7bee585f649b57722eb32ca
Moka Yomoto <psp2ipso2moka@gmail.com>
2017-10-21 16:16:09 +0900

    Add Scripts

---
+++ Assets/Scripts/N_Sample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class N_Sample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("kita!");
+	}
+}
