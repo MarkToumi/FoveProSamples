abf00ad35674f44ad5cc7c67b03e1818d1ad6162
Takuma Nagata <takuma.fn@gmail.com>
2017-10-24 15:46:09 +0900

    Add Enemy.cs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	private Collider _collider;
+	// Use this for initialization
+	void Start () {
+		_collider = this.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(FoveInterface.IsLookingAtCollider(_collider)) {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
